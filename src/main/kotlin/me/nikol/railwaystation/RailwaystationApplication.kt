dfd5e91732de5ab69e483331156a81f8bf1cd276
ProfessorNik <90216842+ProfessorNik@users.noreply.github.com>
2023-02-15 23:30:51 +0700

    first commit

---
+++ src/main/kotlin/me/nikol/railwaystation/RailwaystationApplication.kt
@@ -0,0 +1,11 @@
+package me.nikol.railwaystation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RailwaystationApplication
+
+fun main(args: Array<String>) {
+	runApplication<RailwaystationApplication>(*args)
+}
---
+++ src/test/kotlin/me/nikol/railwaystation/RailwaystationApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.nikol.railwaystation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RailwaystationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
